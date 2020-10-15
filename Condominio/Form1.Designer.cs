6e81abb8f644d4a8d90353ee47e1cd5159860d72
Bruno Silva <b.santos.slm@gmail.com>
2020-10-15 04:08:49 -0300

    first commit

---
+++ Condominio/Form1.Designer.cs
@@ -0,0 +1,354 @@
+﻿namespace Condominio {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.mtbTelefone = new System.Windows.Forms.MaskedTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbApartamento = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbBloco = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.mtbAniversario = new System.Windows.Forms.MaskedTextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tbNome = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnExibir = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvDados = new System.Windows.Forms.DataGridView();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnLimpar);
+            this.groupBox1.Controls.Add(this.tbEmail);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.mtbTelefone);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.tbApartamento);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.tbBloco);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.mtbAniversario);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.tbNome);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.tbId);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 81);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(440, 184);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cadastro e Consulta";
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(87, 149);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(264, 20);
+            this.tbEmail.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(15, 152);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(39, 13);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "E-Mail:";
+            // 
+            // mtbTelefone
+            // 
+            this.mtbTelefone.Location = new System.Drawing.Point(87, 123);
+            this.mtbTelefone.Mask = "(00) 00000-0000";
+            this.mtbTelefone.Name = "mtbTelefone";
+            this.mtbTelefone.Size = new System.Drawing.Size(116, 20);
+            this.mtbTelefone.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(10, 123);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(52, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Telefone:";
+            // 
+            // tbApartamento
+            // 
+            this.tbApartamento.Location = new System.Drawing.Point(273, 14);
+            this.tbApartamento.Name = "tbApartamento";
+            this.tbApartamento.Size = new System.Drawing.Size(43, 20);
+            this.tbApartamento.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(205, 17);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Apartamento:";
+            // 
+            // tbBloco
+            // 
+            this.tbBloco.Location = new System.Drawing.Point(156, 14);
+            this.tbBloco.Name = "tbBloco";
+            this.tbBloco.Size = new System.Drawing.Size(43, 20);
+            this.tbBloco.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(113, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Bloco:";
+            // 
+            // mtbAniversario
+            // 
+            this.mtbAniversario.Location = new System.Drawing.Point(87, 95);
+            this.mtbAniversario.Mask = "00/00";
+            this.mtbAniversario.Name = "mtbAniversario";
+            this.mtbAniversario.Size = new System.Drawing.Size(68, 20);
+            this.mtbAniversario.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Aniversário:";
+            // 
+            // tbNome
+            // 
+            this.tbNome.Location = new System.Drawing.Point(87, 69);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(286, 20);
+            this.tbNome.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 72);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nome:";
+            // 
+            // tbId
+            // 
+            this.tbId.Location = new System.Drawing.Point(87, 43);
+            this.tbId.Name = "tbId";
+            this.tbId.Size = new System.Drawing.Size(66, 20);
+            this.tbId.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Id:";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btnRegistrar.Location = new System.Drawing.Point(30, 271);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistrar.TabIndex = 1;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // btnAtualizar
+            // 
+            this.btnAtualizar.Location = new System.Drawing.Point(111, 270);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(75, 23);
+            this.btnAtualizar.TabIndex = 2;
+            this.btnAtualizar.Text = "Atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
+            // 
+            // btnExibir
+            // 
+            this.btnExibir.Location = new System.Drawing.Point(192, 270);
+            this.btnExibir.Name = "btnExibir";
+            this.btnExibir.Size = new System.Drawing.Size(75, 23);
+            this.btnExibir.TabIndex = 3;
+            this.btnExibir.Text = "Exibir";
+            this.btnExibir.UseVisualStyleBackColor = true;
+            this.btnExibir.Click += new System.EventHandler(this.btnExibir_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(273, 270);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 4;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.ForeColor = System.Drawing.Color.Red;
+            this.btnExcluir.Location = new System.Drawing.Point(354, 270);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 5;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(298, 35);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Registro de Moradores";
+            // 
+            // dgvDados
+            // 
+            this.dgvDados.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.dgvDados.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDados.GridColor = System.Drawing.SystemColors.Control;
+            this.dgvDados.Location = new System.Drawing.Point(458, 9);
+            this.dgvDados.Name = "dgvDados";
+            this.dgvDados.Size = new System.Drawing.Size(640, 285);
+            this.dgvDados.TabIndex = 7;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(8, 49);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(146, 19);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Condomínio Norteano";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(316, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(116, 69);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(371, 14);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(63, 19);
+            this.btnLimpar.TabIndex = 14;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1108, 304);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.dgvDados);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnExibir);
+            this.Controls.Add(this.btnAtualizar);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDados)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnExibir;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbNome;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbId;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.MaskedTextBox mtbTelefone;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbApartamento;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbBloco;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox mtbAniversario;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dgvDados;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnLimpar;
+    }
+}
+
---
+++ Condominio/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Condominio {
+    public partial class Form1 : Form {
+
+        SqlConnection conexao;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        SqlDataAdapter da;
+
+        string strSql;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e) {
+            try {
+                conexao = new SqlConnection(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=bruno;Data Source=SANTOS");
+                strSql = "INSERT INTO CONDOMINIO (ID, NOME, ANIVERSARIO, BLOCO, APARTAMENTO, TELEFONE, EMAIL) VALUES (@ID, @NOME, @ANIVERSARIO, @BLOCO, @APARTAMENTO, @TELEFONE, @EMAIL)";
+
+                cmd = new SqlCommand(strSql, conexao);
+
+                cmd.Parameters.AddWithValue("@ID", tbId.Text);
+                cmd.Parameters.AddWithValue("@NOME", tbNome.Text);
+                cmd.Parameters.AddWithValue("@ANIVERSARIO", mtbAniversario.Text);
+                cmd.Parameters.AddWithValue("@BLOCO", tbBloco.Text);
+                cmd.Parameters.AddWithValue("@APARTAMENTO", tbApartamento.Text);
+                cmd.Parameters.AddWithValue("@TELEFONE", mtbTelefone.Text);
+                cmd.Parameters.AddWithValue("@EMAIL", tbEmail.Text);
+
+                conexao.Open();
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Morador Registrado com Sucesso");
+                tbId.Text = null;
+                tbNome.Text = null;
+                mtbAniversario.Text = null;
+                tbBloco.Text = null;
+                tbApartamento.Text = null;
+                mtbTelefone.Text = null;
+                tbEmail.Text = null;
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+            finally {
+                conexao.Close();
+                conexao = null;
+                cmd = null;
+            }
+            
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+        }
+
+        private void btnExibir_Click(object sender, EventArgs e) {
+            try {
+                conexao = new SqlConnection(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=bruno;Data Source=SANTOS");
+                strSql = "SELECT * FROM CONDOMINIO";
+
+                cmd = new SqlCommand(strSql, conexao);
+
+                DataSet ds = new DataSet();
+                da = new SqlDataAdapter(strSql, conexao);
+                
+                conexao.Open();
+
+                da.Fill(ds);
+
+                dgvDados.DataSource = ds.Tables[0];
+                /*dr = cmd.ExecuteReader();
+
+
+
+                while(dr.Read()){
+
+                    tbNome.Text = (string)dr["NOME"];
+                    mtbAniversario.Text = (string)dr["ANIVERSARIO"];
+                    tbBloco.Text = (string)dr["BLOCO"];
+                    
+                }*/
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+
+            }
+            finally {
+                conexao.Close();
+                conexao = null;
+                cmd = null;
+            }
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e) {
+            try {
+                conexao = new SqlConnection(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=bruno;Data Source=SANTOS");
+                strSql = "SELECT * FROM CONDOMINIO WHERE APARTAMENTO = @APARTAMENTO OR ID = @ID";
+
+                cmd = new SqlCommand(strSql, conexao);
+
+                cmd.Parameters.AddWithValue("@APARTAMENTO", tbApartamento.Text);
+                cmd.Parameters.AddWithValue("@ID", tbId.Text);
+
+                conexao.Open();
+                dr = cmd.ExecuteReader();
+
+                while (dr.Read()) {
+                    tbBloco.Text = (string)dr["BLOCO"];
+                    tbApartamento.Text = (string)dr["APARTAMENTO"];
+                    tbId.Text = Convert.ToString(dr["ID"]);
+                    tbNome.Text = (string)dr["NOME"];
+                    mtbAniversario.Text = (string)dr["ANIVERSARIO"];
+                    mtbTelefone.Text = (string)dr["TELEFONE"];
+                    tbEmail.Text = (string)dr["EMAIL"];
+
+                }
+
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+            finally {
+                conexao.Close();
+                conexao = null;
+                cmd = null;
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e) {
+            tbApartamento.Text = null;
+            tbBloco.Text = null;
+            tbEmail.Text = null;
+            tbId.Text = null;
+            tbNome.Text = null;
+            mtbTelefone.Text = null;
+            mtbAniversario.Text = null;
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e) {
+            try {
+                conexao = new SqlConnection(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=bruno;Data Source=SANTOS");
+                strSql = "UPDATE CONDOMINIO SET NOME=@NOME, ANIVERSARIO=@ANIVERSARIO, BLOCO=@BLOCO, APARTAMENTO=@APARTAMENTO, TELEFONE=@TELEFONE, EMAIL=@EMAIL WHERE ID=@ID";
+
+                cmd = new SqlCommand(strSql, conexao);
+
+                cmd.Parameters.AddWithValue("@ID", tbId.Text);
+                cmd.Parameters.AddWithValue("@NOME", tbNome.Text);
+                cmd.Parameters.AddWithValue("@ANIVERSARIO", mtbAniversario.Text);
+                cmd.Parameters.AddWithValue("@BLOCO", tbBloco.Text);
+                cmd.Parameters.AddWithValue("@APARTAMENTO", tbApartamento.Text);
+                cmd.Parameters.AddWithValue("@TELEFONE", mtbTelefone.Text);
+                cmd.Parameters.AddWithValue("@EMAIL", tbEmail.Text);
+
+                conexao.Open();
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Morador Atualizado com Sucesso");
+                tbId.Text = null;
+                tbNome.Text = null;
+                mtbAniversario.Text = null;
+                tbBloco.Text = null;
+                tbApartamento.Text = null;
+                mtbTelefone.Text = null;
+                tbEmail.Text = null;
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+            finally {
+                conexao.Close();
+                conexao = null;
+                cmd = null;
+            }
+
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e) {
+            try {
+                conexao = new SqlConnection(@"Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=bruno;Data Source=SANTOS");
+                strSql = "DELETE CONDOMINIO WHERE ID=@ID";
+
+                cmd = new SqlCommand(strSql, conexao);
+
+                cmd.Parameters.AddWithValue("@ID", tbId.Text);
+
+                conexao.Open();
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Registro Excluído!");
+                tbId.Text = null;
+                tbNome.Text = null;
+                mtbAniversario.Text = null;
+                tbBloco.Text = null;
+                tbApartamento.Text = null;
+                mtbTelefone.Text = null;
+                tbEmail.Text = null;
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+            finally {
+                conexao.Close();
+                conexao = null;
+                cmd = null;
+            }
+        }
+    }
+}
